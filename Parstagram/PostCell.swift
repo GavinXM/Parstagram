9ccebdf426cff744cafd48bd56cce1bcab346879
Gavin McNatt <gmcnatt14@students.desu.edu@gavins-macbook-pro.local>
2019-03-05 23:15:15 -0500

    Final

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Gavin McNatt on 3/5/19.
+//  Copyright © 2019 Gavin McNatt. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
